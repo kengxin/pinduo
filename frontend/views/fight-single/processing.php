92797f9b524508731c143bd20573f579b4fa28aa
xin <137105161@qq.com>
2017-12-15 17:30:17 +0800

    1.修复bug

--- frontend/views/fight-single/processing.php
+++ frontend/views/fight-single/processing.php
@@ -191,7 +191,7 @@
         <div class="weui-dialog__bd">
             <p class="weui-prompt-text">记录您的信息保存您的拼团信息</p>
             <input type="text" class="weui-input user-name weui-prompt-input" id="weui-prompt-input" value="" placeholder="姓名">
-            <input type="number" class="weui-input tel weui-prompt-input" id="weui-prompt-input" value="" placeholder="手机号码">
+            <input type="number" class="weui-input tel weui-prompt-input" id="weui-prompt-input-two" value="" placeholder="手机号码">
         </div>
         <div class="weui-dialog__ft" style="border-top: none">
             <a href="javascript:;" class="weui-dialog__btn default onok" style="background: #fd537b;color: white;width: 70%;height: 40px;line-height: 40px;margin: 0 30px 20px 30px;border-radius: 30px;">加入拼单</a>
