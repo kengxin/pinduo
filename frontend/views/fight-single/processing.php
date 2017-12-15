36a14f7427c3520f8174b4f3198f90094cbbe816
xin <137105161@qq.com>
2017-12-15 14:05:03 +0800

    1.修复bug

--- frontend/views/fight-single/processing.php
+++ frontend/views/fight-single/processing.php
@@ -417,7 +417,7 @@ var enter = false;
 var hiddenProperty = 'hidden' in document ? 'hidden' : 'webkitHidden' in document ? 'webkitHidden' : 'mozHidden' in document ? 'mozHidden' : null;
 var visibilityChangeEvent = hiddenProperty.replace(/hidden/i, 'visibilitychange');
 var onVisibilityChange = function(){
-    if (!document[hiddenProperty] && enter == false) {
+    if (!document[hiddenProperty] && enter == false && isJoin == 1) {
         $('.save_address').show();
         $('#speBg').show()
     }
