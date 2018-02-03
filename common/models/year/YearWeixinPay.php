11b0484197c83bb6e2d6bffc5ce15f500044cfa0
xin <137105161@qq.com>
2018-02-03 10:41:05 +0800

    1.修复bug

--- common/models/year/YearWeixinPay.php
+++ common/models/year/YearWeixinPay.php
@@ -64,8 +64,6 @@ class YearWeixinPay extends ActiveRecord
 
         if ($this->save()) {
             return Yii::$app->yearWeixinPay->pay(Yii::$app->yearUser->getOpenId(), $this->id + 1000000, $info, $total_fee);
-        } else {
-            var_dump($this->getErrors());die;
         }
 
         return false;
