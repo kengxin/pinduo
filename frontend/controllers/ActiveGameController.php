b1d9d58fe51a8f8da14c5143d6419ed74477a81d
xin <137105161@qq.com>
2018-02-05 14:34:29 +0800

    1.修复bug

--- frontend/controllers/ActiveGameController.php
+++ frontend/controllers/ActiveGameController.php
@@ -188,7 +188,7 @@ class ActiveGameController extends Controller
                         return json_encode([
                             'code' => 0,
                             'data' => [
-                                'show' => 1,
+                                'show' => 0,
                                 'game_id' => $gameLog->id,
                                 'questionInfo' => $questionInfo
                             ]
